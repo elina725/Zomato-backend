55e53d407b45fae5ea5cce6030ad24d99073aa39
elina725 <elinadecember8@gmail.com>
2026-07-13 04:01:03 +0530

    Use Railway MySQL

--- Confiq/db.js
+++ Confiq/db.js
@@ -1,11 +1,8 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const db = new Sequelize('zamato_db', 'root', 'Elina725@', {
-  host: 'localhost',
-  dialect: 'mysql',
+const db = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
   logging: false,
 });
-
- 
 
 module.exports = db;
